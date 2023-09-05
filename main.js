823f18a6977ef3f6c97d1b81227dfff573e60133
Oceangrad <102550817+Oceangrad@users.noreply.github.com>
2023-09-06 01:00:06 +0300

    Added header to project

--- main.js
+++ main.js
@@ -11,3 +11,9 @@ $(".content-box").mouseleave(function(){
     $(this).find("img").stop();
     $(this).find("img").animate({scale: "1"}, 'fast');
 })
+
+$(".mid-header-block button").mouseenter(function(){
+    console.log(this)
+    $(this).stop();
+    $(this).animate({backgroundColor: "rgb(0, 255, 255)"}, 'fast');
+})
