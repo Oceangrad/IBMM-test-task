748f687fe0975eb7ed5f40826241ade5430559b6
Oceangrad <102550817+Oceangrad@users.noreply.github.com>
2023-09-05 21:25:25 +0300

    fixed animations

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ $(document).ready(function(){
 
 $(".content-box").mouseenter(function(){
     $(this).find("img").stop();
-    $(this).find("img").animate({scale: "1.2"}, 'slow');
+    $(this).find("img").animate({scale: "1.1"}, 'fast');
 })
 
 $(".content-box").mouseleave(function(){
     $(this).find("img").stop();
-    $(this).find("img").animate({scale: "1"}, 'slow');
+    $(this).find("img").animate({scale: "1"}, 'fast');
 })
